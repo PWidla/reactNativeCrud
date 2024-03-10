067ab611c433d4333fedf5426a48bf7f6f418ff6
Patryk <patrykwidlazb@gmail.com>
2024-03-10 14:09:05 +0100

    wip navigation

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Link, router } from "expo-router";
+import { Pressable, Text, View } from "react-native";
+
+const HomePage = () => {
+  return (
+    <View>
+      <Text>Home page</Text>
+      <Link href="/users/1">Go to user 1</Link>
+      <Pressable onPress={() => router.push("/users/2")}>
+        <Text>Go to user 2</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default HomePage;
